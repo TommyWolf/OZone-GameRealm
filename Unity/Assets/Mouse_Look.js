68c0a4186c37c2a49d8ed8241454ef34ef974116
SKSKSUnite <afecto.joao@gmail.com>
2014-01-30 15:07:55 +0000

    Unity Some Moar Stuff ====================== Well, well, well...

---
+++ Unity/Assets/Mouse_Look.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var rotateSpeed = 5.0;
+var hitdist = 28.0;
+ 
+function Update () 
+ 
+{   
+    var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+    var targetPoint = ray.GetPoint(hitdist);
+    var targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+ 
+    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation,
+    rotateSpeed * Time.deltaTime);
+}
