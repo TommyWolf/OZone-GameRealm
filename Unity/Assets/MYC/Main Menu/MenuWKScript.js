6cbf7a0087ed78d886780993d0b0787651fb6a19
SKSKSUnite <afecto.joao@gmail.com>
2014-02-25 14:38:39 +0000

    Unity Multiplayer =================== Now, let's setup a auth server. Anyway, stay tuned for the updates for new maps or other stuff.

--- Unity/Assets/MYC/Main Menu/MenuWKScript.js
+++ Unity/Assets/MYC/Main Menu/MenuWKScript.js
@@ -1,12 +1,13 @@
-﻿	public var facebookUser : String = "Enter your FB Login";
-	public var facebookAuth : String = "Enter your FB Password";
+﻿
+	public var authName : String = "Enter your FB Login";
+	public var authToken : String = "Enter your FB Password";
 	public var windowTitle = "Connect to Master";
 	public var windowRect1 = 30;
 	public var windowRect2 = 30;
 	public var windowRect3 = 120;
 	public var windowRect4 = 50;
 	public var windowRect : Rect = Rect (windowRect1, windowRect2, windowRect3, windowRect4);
-	// vars with hide don't show up in inspector. at least thats the plan.
+	// vars with hide don't show up in inspector. at least that's the plan.
 	public var windowNickField1 = 20;
 	public var windowNickField2 = 15;
 	public var windowNickField3 = 110;
@@ -20,14 +21,22 @@
 	public var windowConnectRect3 = 90;
 	public var windowConnectRect4 = 35;
 	public var ConnectButtonText = "Connect";
+	
+function Connect()
+	{
+	PhotonNetwork.AuthValues = new AuthenticationValues();
+	PhotonNetwork.AuthValues.SetAuthParameters(authName, authToken);
+	PhotonNetwork.ConnectUsingSettings("0.1");
+	}
 
-	function Window0() {
-	facebookUser = GUI.TextField (Rect (windowNickField1, windowNickField2, windowNickField3, windowNickField4), facebookUser, 25);
-	facebookAuth = GUI.TextField (Rect (windowPassField1, windowPassField2, windowPassField3, windowPassField4), facebookAuth, 25);
+function Window0() {
+	authName = GUI.TextField (Rect (windowNickField1, windowNickField2, windowNickField3, windowNickField4), authName, 25);
+		
+	authToken = GUI.TextField (Rect (windowPassField1, windowPassField2, windowPassField3, windowPassField4), authToken, 25);
 	
 	
 	if (GUI.Button(Rect (windowConnectRect1, windowConnectRect2, windowConnectRect3, windowConnectRect4), ConnectButtonText))
-		Debug.Log(facebookUser);
+		Connect();
 
 }
 function OnGUI() {
@@ -35,3 +44,6 @@ function OnGUI() {
 	(GUI.Window (0, windowRect, Window0, windowTitle));
 	
 	}
+	
+
+
